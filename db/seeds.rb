980c4783dee16a1fadfb953a35ff1677b81dd3b7
Hugo Lavorel <hugo.lavorel@gmail.com>
2020-08-13 16:15:10 +0200

    Added cocktails index and show and dose management

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+Cocktail.destroy_all
+Dose.destroy_all
+Ingredient.destroy_all
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+buffer = open(url).read
+result = JSON.parse(buffer)
+
+result['drinks'].each do |ingredient|
+  Ingredient.create(name: ingredient['strIngredient1'])
+end
+
+puts 'Seed done!'
